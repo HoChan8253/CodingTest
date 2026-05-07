ce277808540e2e1f17f70645f1b503a32faedf26
HoChan <nahcohochan@gmail.com>
2026-05-08 04:36:34 +0900

    [level 0] Title: 배열의 평균값, Time: 0.12 ms, Memory: 30.8 MB -BaekjoonHub

--- C#/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
+++ C#/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -1,9 +1,11 @@
 using System;
+//using System.Linq;
 
 public class Solution 
 {
     public double solution(int[] numbers) 
     {
+        // 풀이 1 - foreach 사용
         int result = 0;
         double answer = 0;
         foreach(int sum in numbers)
@@ -12,5 +14,8 @@ public class Solution
         }
         answer = result / (double)numbers.Length;
         return answer;
+        
+        // 풀이 2 - LINQ 사용
+        //return numbers.Average();
     }
 }
