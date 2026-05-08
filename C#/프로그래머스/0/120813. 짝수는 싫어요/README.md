9719fbc5a8635d0e5e620a3363af1e5a13366f52
HoChan <nahcohochan@gmail.com>
2026-05-09 01:14:03 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.47 ms, Memory: 30.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120813. 짝수는 싫어요/README.md
+++ C#/프로그래머스/0/120813. 짝수는 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수는 싫어요 - 120813 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813?language=csharp) 
 
 ### 성능 요약
 
-메모리: 30.8 MB, 시간: 0.14 ms
+메모리: 30.5 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 09일 01:05:23
+2026년 05월 09일 01:14:01
 
 ### 문제 설명
 
--- C#/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
+++ C#/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution 
 {
     public int[] solution(int n) 
     {
-        int[] answer = new int[(n + 1) / 2];
-        int idx = 0;
+        // 풀이 1 - Array 사용
+        
+        //int[] answer = new int[(n + 1) / 2];
+        //int idx = 0;
+        //for(int i = 1; i <= n; i++)
+        //{
+        //   if(i % 2 != 0)
+        //    {
+        //        answer[idx] = i;
+        //        idx++;
+        //    }
+        //}
+        //return answer;
+        
+        // 풀이 2 - List 사용
+        List<int> list = new List<int>();
+        
         for(int i = 1; i <= n; i++)
         {
-            if(i % 2 != 0)
-            {
-                answer[idx] = i;
-                idx++;
-            }
+            if(i % 2 != 0) list.Add(i);
         }
-        return answer;
+        
+        return list.ToArray();
     }
 }
