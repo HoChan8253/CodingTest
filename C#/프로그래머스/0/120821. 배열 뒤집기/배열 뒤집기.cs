dc25fc5874331e98a9ad0e3ddce61beead7c6eea
HoChan <nahcohochan@gmail.com>
2026-05-10 15:04:46 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.13 ms, Memory: 31 MB -BaekjoonHub

--- C#/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
+++ C#/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -1,9 +1,11 @@
 using System;
+//using Linq;
 
 public class Solution 
 {
     public int[] solution(int[] num_list) 
     {
+        // 풀이 1 - Array 사용
         int[] answer = new int[num_list.Length];
         int c = num_list.Length - 1;
         foreach(int rvs in num_list)
@@ -12,5 +14,8 @@ public class Solution
             c--;
         }
         return answer;
+        
+        // 풀이 2 - LINQ 사용
+        //return num_list.Reverse().ToArray();
     }
 }
