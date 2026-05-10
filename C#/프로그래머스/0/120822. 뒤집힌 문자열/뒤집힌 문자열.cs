d8d92c5326207dfe7bab1126820e2c889fb81e90
HoChan <nahcohochan@gmail.com>
2026-05-10 15:21:09 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.50 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cs
+++ C#/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cs
@@ -4,11 +4,17 @@ public class Solution
 {
     public string solution(string my_string) 
     {
-        string answer = "";
-        foreach(char c in my_string)
-        {
-            answer = c + answer;
-        }
-        return answer;
+        //풀이 1 - foreach 사용
+        //string answer = "";
+        //foreach(char c in my_string)
+        //{
+        //    answer = c + answer;
+        //}
+        //return answer;
+        
+        // 풀이 2 - Array 사용
+        char[] arr = my_string.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
     }
 }
