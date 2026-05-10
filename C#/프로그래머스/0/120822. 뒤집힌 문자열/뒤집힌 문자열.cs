883f9ec3eb7f156a795977aff29c205b0161d04b
HoChan <nahcohochan@gmail.com>
2026-05-10 15:16:21 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.17 ms, Memory: 30.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution 
+{
+    public string solution(string my_string) 
+    {
+        string answer = "";
+        foreach(char c in my_string)
+        {
+            answer = c + answer;
+        }
+        return answer;
+    }
+}
