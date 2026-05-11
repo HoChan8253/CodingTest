a1d765029ea58c8b7c4ef42db578e4f56797873c
HoChan <nahcohochan@gmail.com>
2026-05-12 03:12:14 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 9.76 ms, Memory: 30.6 MB -BaekjoonHub

--- C#/프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
+++ C#/프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -4,13 +4,21 @@ public class Solution
 {
     public long solution(int a, int b) 
     {
+        // 풀이 1 - 반복문 사용
         long answer = 0;
         
         for(int i = Math.Min(a, b); i <= Math.Max(a, b); i++)
         {
-            answer += i;
+           answer += i;
         }
         
         return answer;
+        
+        // 풀이 2 - 등차수열 사용
+        //long min = Math.Min(a, b);
+        //long max = Math.Max(a, b);
+        //return (min + max) * (max - min + 1) / 2;
+        
+        //return ((long)(Math.Abs(a - b) + 1) * (a + b)) / 2;
     }
 }
