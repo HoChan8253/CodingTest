cbb51c3b301b99f49e4f95e18bb3c1047d4c2728
HoChan <nahcohochan@gmail.com>
2026-05-11 18:50:14 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.44 ms, Memory: 30.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
+++ C#/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -1,18 +1,23 @@
 using System;
+using System.Linq;
 
 public class Solution 
 {
     public string solution(string my_string, string letter) 
     {
-        string answer = "";
-        foreach(char c in my_string)
-        {
-            if(c.ToString() == letter)
-            {
-                continue;
-            }
-            answer += c;
-        }
-        return answer;
+        // 풀이 1 - foreach 사용
+        //string answer = "";
+        //foreach(char c in my_string)
+        //{
+        //    if(c.ToString() == letter)
+        //    {
+        //        continue;
+        //    }
+        //    answer += c;
+        //}
+        //return answer;
+        
+        // 풀이 2 - LINQ 사용
+        return my_string.Replace(letter, "");
     }
 }
