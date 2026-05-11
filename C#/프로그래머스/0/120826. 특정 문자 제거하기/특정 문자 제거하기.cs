86a30033855005ecb0d8c791c941a4e483c090b4
HoChan <nahcohochan@gmail.com>
2026-05-11 18:47:55 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.31 ms, Memory: 30.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution 
+{
+    public string solution(string my_string, string letter) 
+    {
+        string answer = "";
+        foreach(char c in my_string)
+        {
+            if(c.ToString() == letter)
+            {
+                continue;
+            }
+            answer += c;
+        }
+        return answer;
+    }
+}
