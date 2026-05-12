3caa7f92b3975b861aac885932af4ac71ab163bf
HoChan <nahcohochan@gmail.com>
2026-05-13 08:55:56 +0900

    [level 0] Title: 개미 군단, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int hp) 
+    {
+        int general = hp / 5;
+        int soldier = hp % 5 / 3;
+        int worker = hp % 5 % 3 / 1;
+        int answer = general + soldier + worker;
+        return answer;
+    }
+}
