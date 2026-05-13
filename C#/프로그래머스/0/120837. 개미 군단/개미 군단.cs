e17c9d2943c8a503fe4116a580fdff00d95a2f3d
HoChan <nahcohochan@gmail.com>
2026-05-13 09:00:15 +0900

    [level 0] Title: 개미 군단, Time: 0.14 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/120837. 개미 군단/개미 군단.cs
+++ C#/프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -4,10 +4,22 @@ public class Solution
 {
     public int solution(int hp) 
     {
-        int general = hp / 5;
-        int soldier = hp % 5 / 3;
-        int worker = hp % 5 % 3;
-        int answer = general + soldier + worker;
+        // 풀이 1 - 반복문 없음
+        //int general = hp / 5;
+        //int soldier = hp % 5 / 3;
+        //int worker = hp % 5 % 3;
+        //int answer = general + soldier + worker;
+        //return answer;
+        
+        // 풀이 2 - 반복문 사용
+        int answer = 0;
+        while(hp > 0)
+        {
+            if(hp >= 5) hp -= 5;
+            else if(hp >= 3) hp -= 3;
+            else hp -= 1;
+            answer++;
+        }
         return answer;
     }
 }
