5afbad1a25393d3dad1d105c7b2a7e9c1ce76bce
HoChan <nahcohochan@gmail.com>
2026-05-16 01:30:12 +0900

    [level 0] Title: 배열의 유사도, Time: 0.21 ms, Memory: 30.6 MB -BaekjoonHub

--- C#/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
+++ C#/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
@@ -5,12 +5,28 @@ public class Solution
 {
     public int solution(string[] s1, string[] s2) 
     {
+        // 풀이 1 - LINQ 사용
+        //int answer = 0;
+        //for(int i = 0; i < s2.Length; i++)
+        //{
+        //    if(s1.Contains(s2[i]))
+        //    {
+        //        answer++;
+        //   }
+        //}
+        //return answer;
+        
+        // 풀이 2 - 반복문 사용
         int answer = 0;
-        for(int i = 0; i < s2.Length; i++)
+        for(int i = 0; i < s1.Length; i++)
         {
-            if(s1.Contains(s2[i]))
+            for(int j = 0; j < s2.Length; j++)
             {
-                answer++;
+                if(s1[i] == s2[j])
+                {
+                    answer++;
+                    break;
+                }
             }
         }
         return answer;
