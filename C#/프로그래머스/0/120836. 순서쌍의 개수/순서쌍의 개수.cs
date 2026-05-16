0f415c252235d8e52a9f5e752eafb56a990bc39b
HoChan <nahcohochan@gmail.com>
2026-05-17 08:08:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.16 ms, Memory: 30.6 MB -BaekjoonHub

--- C#/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cs
+++ C#/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cs
@@ -4,12 +4,31 @@ public class Solution
 {
     public int solution(int n) 
     {
+        // 풀이 1
+        //int answer = 0;
+        //for(int i = 1; i <= n; i++)
+        //{
+        //    if(n % i == 0)
+        //    {
+        //        answer++;
+        //    }
+        //}
+        //return answer;
+        
+        // 풀이 2
         int answer = 0;
-        for(int i = 1; i <= n; i++)
+        for(int i = 1; i <= Math.Sqrt(n); i++)
         {
             if(n % i == 0)
             {
-                answer++;
+                if(i == n / i)
+                {
+                    answer++;
+                }
+                else
+                {
+                    answer += 2;
+                }
             }
         }
         return answer;
