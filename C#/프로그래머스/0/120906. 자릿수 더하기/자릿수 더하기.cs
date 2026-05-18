5c9ff6183ef7370283690667c3999d80bba3c4e4
HoChan <nahcohochan@gmail.com>
2026-05-18 14:30:26 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 31 MB -BaekjoonHub

--- C#/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cs
+++ C#/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cs
@@ -4,11 +4,21 @@ public class Solution
 {
     public int solution(int n) 
     {
+        // 풀이 1
+        //int answer = 0;
+        //string s = n.ToString();
+        //foreach(char c in s)
+        //{
+        //    answer += c - '0';
+        //}
+        //return answer;
+        
+        // 풀이 2
         int answer = 0;
-        string s = n.ToString();
-        foreach(char c in s)
+        while(n > 0)
         {
-            answer += c - '0';
+            answer += n % 10;
+            n /= 10;
         }
         return answer;
     }
