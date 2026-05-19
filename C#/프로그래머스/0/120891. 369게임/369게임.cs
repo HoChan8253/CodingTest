835890e2b4c1a914ac429628574de9394571b99a
HoChan <nahcohochan@gmail.com>
2026-05-19 15:40:55 +0900

    [level 0] Title: 369게임, Time: 0.19 ms, Memory: 30.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120891. 369게임/369게임.cs
+++ C#/프로그래머스/0/120891. 369게임/369게임.cs
@@ -4,10 +4,24 @@ public class Solution
 {
     public int solution(int order) 
     {
+        // 풀이 1
+        //int answer = 0;
+        //while(order > 0)
+        //{
+        //    if((order % 10) % 3 == 0 && order % 10 != 0)
+        //    {
+        //        answer++;
+        //    }
+        //    order = order / 10;
+        //}
+        //return answer;
+        
+        // 풀이 2
         int answer = 0;
         while(order > 0)
         {
-            if((order % 10) % 3 == 0 && order % 10 != 0)
+            int digit = order % 10;
+            if(digit == 3 || digit == 6 || digit == 9)
             {
                 answer++;
             }
