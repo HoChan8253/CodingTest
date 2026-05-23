f975d72e8fa349b2aba4d012b2c48cdf9437d413
HoChan <nahcohochan@gmail.com>
2026-05-23 11:34:23 +0900

    [level 0] Title: 배열 자르기, Time: 0.11 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120833. 배열 자르기/배열 자르기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int[] numbers, int num1, int num2) 
+    {
+        int[] answer = new int[num2 - num1 + 1];
+        for(int i = 0; i <= num2 - num1; i++)
+        {
+            answer[i] = numbers[i + num1];
+        }
+        return answer;
+    }
+}
