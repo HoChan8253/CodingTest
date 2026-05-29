b74d54bc9758734ffc33dabfc861ab906b576bea
HoChan <nahcohochan@gmail.com>
2026-05-30 00:43:16 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.38 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,12 @@
+public class Solution 
+{
+    public string solution(string[] seoul) 
+    {
+        int index = 0;
+        for(int i = 0; i < seoul.Length; i++)
+        {
+            if(seoul[i] == "Kim") index = i;
+        }
+        return $"김서방은 {index}에 있다";
+    }
+}
