f34096e23a8e75cba93b774e05c52a1b85b99207
HoChan <nahcohochan@gmail.com>
2026-05-30 00:59:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 35.02 ms, Memory: 47 MB -BaekjoonHub

--- C#/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
+++ C#/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -5,8 +5,7 @@ public class Solution
         string answer = "";
         for(int i = 1; i <= n; i++)
         {
-            if(i % 2 != 0) answer += "수";
-            else answer += "박";
+            answer += i % 2 != 0 ? "수" : "박";
         }
         return answer;
     }
