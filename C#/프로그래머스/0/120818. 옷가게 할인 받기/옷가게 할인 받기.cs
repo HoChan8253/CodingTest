b3bb7d7b306daa22aa0139a1a019345a96d4b9f5
HoChan <nahcohochan@gmail.com>
2026-06-01 03:36:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
+++ C#/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
@@ -4,10 +4,9 @@ public class Solution
 {
     public int solution(int price)
     {
-        float offPrice = price;
-        if (price >= 500000)      offPrice = price - (price * 0.2f);
-        else if (price >= 300000) offPrice = price - (price * 0.1f);
-        else if (price >= 100000) offPrice = price - (price * 0.05f);
-        return (int)offPrice;
+        int result = price >= 500000 ? price * 80 / 100 :
+                     price >= 300000 ? price * 90 / 100 :
+                     price >= 100000 ? price * 95 / 100 : price;
+        return result;
     }
 }
