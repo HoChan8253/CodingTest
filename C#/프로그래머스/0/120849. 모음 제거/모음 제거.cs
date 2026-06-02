967d7bbdb877ef1331f0ebda4d0628c42519a046
HoChan <nahcohochan@gmail.com>
2026-06-03 01:39:54 +0900

    [level 0] Title: 모음 제거, Time: 0.18 ms, Memory: 30.5 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution 
+{
+    public string solution(string my_string) 
+    {
+        string answer = "";
+        foreach(char c in my_string)
+        {
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+                continue;
+            }
+            answer += c;
+        }
+        return answer;
+    }
+}
