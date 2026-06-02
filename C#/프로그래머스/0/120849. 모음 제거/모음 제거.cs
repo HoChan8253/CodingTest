63feb526e3080ab790d27e3617d026c8eea226f6
HoChan <nahcohochan@gmail.com>
2026-06-03 02:00:37 +0900

    [level 0] Title: 모음 제거, Time: 1.31 ms, Memory: 30.4 MB -BaekjoonHub

--- C#/프로그래머스/0/120849. 모음 제거/모음 제거.cs
+++ C#/프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 public class Solution 
 {
     public string solution(string my_string) 
     {
-        return Regex.Replace(my_string, "[aeiou]", "");
+        // 풀이 1 - foreach, if
+        //string answer = "";
+        //foreach(char c in my_string)
+        //{
+        //    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        //   {
+        //        continue;
+        //   }
+        //    answer += c;
+        //}
+        //return answer;
+        
+        // 풀이 2 - Replace
+        //public string solution(string my_string) 
+        //{
+        //    foreach(char c in "aeiou")
+        //        my_string = my_string.Replace(c.ToString(), "");
+        //    return my_string;
+        //}
+        
+        // 풀이 3 - Contains
+        //string answer = "";
+        //string vowels = "aeiou";
+        //foreach(char c in my_string)
+        //{
+        //    if(!vowels.Contains(c))
+        //    answer += c;
+        //}
+        //return answer;
+        
+        // 풀이 4 - LINQ
+        return new string(my_string.Where(c => !"aeiou".Contains(c)).ToArray());
+        
+        // 풀이 5 - Regex
+        // return Regex.Replace(my_string, "[aeiou]", "");
     }
 }
