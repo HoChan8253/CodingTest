d23e051dffd7e1f30c98f3be39a78f339ffac295
HoChan <nahcohochan@gmail.com>
2026-06-09 19:30:31 +0900

    [level 0] Title: 배열 비교하기, Time: 0.22 ms, Memory: 30.8 MB -BaekjoonHub

--- C#/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cs
+++ C#/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cs
@@ -4,22 +4,15 @@ public class Solution
 {
     public int solution(int[] arr1, int[] arr2) 
     {
-        int answer = 0;
-        int sum1 = 0;
-        int sum2 = 0;
-        if(arr1.Length < arr2.Length) answer = -1;
-        else if(arr1.Length > arr2.Length) answer = 1;
-        else if(arr1.Length == arr2.Length)
+        if (arr1.Length != arr2.Length)
+        return arr1.Length.CompareTo(arr2.Length);
+    
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < arr1.Length; i++)
         {
-            for(int i = 0; i < arr1.Length; i++)
-            {
-                sum1 += arr1[i];
-                sum2 += arr2[i];
-            }
-            if(sum1 > sum2) answer = 1;
-            else if(sum1 < sum2) answer = -1;
-            else answer = 0;
+            sum1 += arr1[i];
+            sum2 += arr2[i];
         }
-        return answer;
+        return sum1.CompareTo(sum2);
     }
 }
