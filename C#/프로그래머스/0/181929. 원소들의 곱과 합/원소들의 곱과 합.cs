f2b8d1e7bc0b34e81e01f0ab697507179252d03d
HoChan <nahcohochan@gmail.com>
2026-06-11 13:58:45 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.93 ms, Memory: 30.6 MB -BaekjoonHub

--- C#/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
+++ C#/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
@@ -1,17 +1,24 @@
 using System;
+using System.Linq;
 
 public class Solution 
 {
     public int solution(int[] num_list) 
     {
-        int answer = 0;
-        int sum = 0;
-        int result1 = 0;
-        int result2 = 1;
-        foreach(int i in num_list) sum += i;
-        result1 = sum * sum;
-        foreach(int j in num_list) result2 *= j;
-        if(result1 > result2) return 1;
-        else return 0;
+        // 풀이 1
+        //int answer = 0;
+        //int sum = 0;
+        //int result1 = 0;
+        //int result2 = 1;
+        //foreach(int i in num_list) sum += i;
+        //result1 = sum * sum;
+        //foreach(int j in num_list) result2 *= j;
+        //if(result1 > result2) return 1;
+        //else return 0;
+        
+        // 풀이 2
+        int sum = num_list.Sum();
+        int product = num_list.Aggregate((acc, x) => acc * x);
+        return product < sum * sum ? 1 : 0;
     }
 }
