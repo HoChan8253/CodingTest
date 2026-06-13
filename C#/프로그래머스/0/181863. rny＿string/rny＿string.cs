2c6295d02fe30ae4a08722eba4666acd24005bf8
HoChan <nahcohochan@gmail.com>
2026-06-13 16:00:46 +0900

    [level 0] Title: rny_string, Time: 0.25 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181863. rny＿string/rny＿string.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+
+public class Solution 
+{
+    public string solution(string rny_string) 
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach(char c in rny_string)
+        {
+            if(c == 'm')
+            {
+                sb.Append("rn");
+            }
+            else sb.Append(c);
+        }
+        return sb.ToString();
+    }
+}
