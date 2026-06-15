19a5f959bc57be4174a517bb0e34491fe1b6fcbe
HoChan <nahcohochan@gmail.com>
2026-06-15 21:11:10 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.08 ms, Memory: 45.7 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public string solution(string my_string, int num1, int num2) 
+    {
+        char[] arr = my_string.ToCharArray();
+        char temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        return new string(arr);     
+    }
+}
