40b706bb3f5902205fbc49fe3473e9cba98e4a13
HoChan <nahcohochan@gmail.com>
2026-06-23 18:29:28 +0900

    [level 0] Title: A 강조하기, Time: 7.81 ms, Memory: 47.7 MB -BaekjoonHub

--- C#/프로그래머스/0/181874. A 강조하기/A 강조하기.cs
+++ C#/프로그래머스/0/181874. A 강조하기/A 강조하기.cs
@@ -4,13 +4,17 @@ public class Solution
 {
     public string solution(string myString) 
     {
-        string answer = "";
-        foreach(char c in myString)
-        {
-            char lower = char.ToLower(c);
-            if(lower == 'a') answer += 'A';
-            else answer += lower;
-        }
-        return answer;
+        // 풀이 1
+        //string answer = "";
+        //foreach(char c in myString)
+        //{
+        //    char lower = char.ToLower(c);
+        //    if(lower == 'a') answer += 'A';
+        //    else answer += lower;
+        //}
+        //return answer;
+        
+        // 풀이 2
+        return myString.ToLower().Replace("a", "A");
     }
 }
