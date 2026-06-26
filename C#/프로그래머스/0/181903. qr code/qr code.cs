55938e8847b2742c06bb8d6922e488a974317755
HoChan <nahcohochan@gmail.com>
2026-06-27 02:13:29 +0900

    [level 0] Title: qr code, Time: 0.54 ms, Memory: 47.5 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181903. qr code/qr code.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution 
+{
+    public string solution(int q, int r, string code) 
+    {
+        string answer = "";
+        for(int i = r; i < code.Length; i += q)
+        {
+            answer += code[i];
+        }
+        return answer;
+    }
+}
