5c52f071d9e6437418c54095d3b6c0551153f824
HoChan <nahcohochan@gmail.com>
2026-07-16 15:03:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 7.72 ms, Memory: 47.4 MB -BaekjoonHub

--- C#/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
+++ C#/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
@@ -6,23 +6,43 @@ public class Solution
     public string solution(string s) 
     {
         // 풀이 1
-        string answer = "";
+        //string answer = "";
+        //int idx = 0;
+        //
+        //foreach(char c in s)
+        //{
+        //    if(c == ' ')
+        //    {
+        //        answer += " ";
+        //        idx = 0;
+        //    }
+        //    else
+        //    {
+        //        if(idx % 2 == 0) answer += char.ToUpper(c);
+        //        else answer += char.ToLower(c);
+        //        idx++;
+        //    }
+        //}
+        //return answer;
+        
+        // 풀이 2
+        StringBuilder answer = new StringBuilder();
         int idx = 0;
         
         foreach(char c in s)
         {
             if(c == ' ')
             {
-                answer += " ";
+                answer.Append(' ');
                 idx = 0;
             }
             else
             {
-                if(idx % 2 == 0) answer += char.ToUpper(c);
-                else answer += char.ToLower(c);
+                if(idx % 2 == 0) answer.Append(char.ToUpper(c));
+                else answer.Append(char.ToLower(c));
                 idx++;
             }
         }
-        return answer;
+        return answer.ToString();
     }
 }
