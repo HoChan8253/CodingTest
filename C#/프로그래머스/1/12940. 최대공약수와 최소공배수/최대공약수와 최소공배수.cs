c786ceb1827f133fc0bdd6fe27b0f1ec800a23a6
HoChan <nahcohochan@gmail.com>
2026-07-21 01:09:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.13 ms, Memory: 46 MB -BaekjoonHub

--- C#/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
+++ C#/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
@@ -1,20 +1,24 @@
 using System;
 
-public class Solution 
+public class Solution
 {
-    public int[] solution(int n, int m) 
+    public int[] solution(int n, int m)
     {
-        int SmallNum = Math.Min(n, m);
-        int GCD = 0; // 최대공약수
-        for(int i = SmallNum; i >= 1; i--)
+        int gcd = GCD(n, m);
+        int lcm = n * m / gcd;
+
+        return new int[] { gcd, lcm };
+    }
+
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
         {
-            if(n % i == 0 && m % i == 0)
-            {
-                GCD = i;
-                break;
-            }
+            int temp = a % b;
+            a = b;
+            b = temp;
         }
-        int LCM = (n * m) / GCD; // 최소공배수
-        return new int[] {GCD, LCM};
+
+        return a;
     }
 }
