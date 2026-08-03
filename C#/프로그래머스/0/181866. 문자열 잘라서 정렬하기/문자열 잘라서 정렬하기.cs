14ed0804cd38a392eb7b6a14558901c45c8dfeb1
HoChan <nahcohochan@gmail.com>
2026-08-04 07:02:55 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 12.83 ms, Memory: 49.9 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution 
+{
+    public string[] solution(string myString) 
+    {
+        string[] answer = myString.Split(
+            'x',
+            StringSplitOptions.RemoveEmptyEntries
+        );
+        Array.Sort(answer);
+        return answer;
+    }
+}
