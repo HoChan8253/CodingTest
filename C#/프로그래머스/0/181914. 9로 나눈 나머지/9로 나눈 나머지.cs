3bf78ae1617a3dd91a91b37c70ba90d174f07905
HoChan <nahcohochan@gmail.com>
2026-08-16 00:13:29 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.48 ms, Memory: 46.8 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution 
+{
+    public int solution(string number) 
+    {
+        int answer = 0;
+        
+        foreach(char c in number)
+        {
+            answer += c - '0';
+        }
+        
+        return answer % 9;
+    }
+}
