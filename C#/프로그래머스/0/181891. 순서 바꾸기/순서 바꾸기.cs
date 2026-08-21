90a36402ed0af55743468e401a0d2aefa7c7ff86
HoChan <nahcohochan@gmail.com>
2026-08-21 14:33:44 +0900

    [level 0] Title: 순서 바꾸기, Time: 2.70 ms, Memory: 46 MB -BaekjoonHub

--- C#/프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cs
+++ C#/프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cs
@@ -1,24 +1,29 @@
 using System;
+using System.Linq;
 
 public class Solution 
 {
     public int[] solution(int[] num_list, int n) 
     {
-        int[] answer = new int[num_list.Length];
-        int index = 0;
+        // 풀이 1 - for문
+        //int[] answer = new int[num_list.Length];
+        //int index = 0;
+        //
+        //for(int i = n; i < num_list.Length; i++)
+        //{
+        //    answer[index] = num_list[i];
+        //    index++;
+        //}
+        //
+        //for(int i = 0; i < n; i++)
+        //{
+        //    answer[index] = num_list[i];
+        //    index++;
+        //}
+        //
+        //return answer;
         
-        for(int i = n; i < num_list.Length; i++)
-        {
-            answer[index] = num_list[i];
-            index++;
-        }
-        
-        for(int i = 0; i < n; i++)
-        {
-            answer[index] = num_list[i];
-            index++;
-        }
-        
-        return answer;
+        // 풀이 2 - LINQ
+        return num_list.Skip(n).Concat(num_list.Take(n)).ToArray();
     }
 }
