0b7dd7cc827f83980ae8dcd59556748cd4988918
HoChan <nahcohochan@gmail.com>
2026-09-01 19:50:45 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.08 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        int pizza = 1;
+        
+        while((pizza * 6) % n != 0)
+        {
+            pizza++;
+        }
+        
+        return pizza;
+    }
+}
